2048d2797f898b5b534084e03b48bf4916dca633
Oteri Prince <oteriprince01@gmail.com>
2024-09-30 13:06:11 +0100

    vite update

--- PinPoint-ui/vite.config.js
+++ PinPoint-ui/vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // This example splits React and React DOM into a separate chunk
+          react: ['react', 'react-dom'],
+          // You can add more chunks for other large dependencies
+          vendor: ['axios', 'lodash'], // Example for other dependencies
+        },
+      },
+    },
+  },
 });
+
+
+
