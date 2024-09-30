93d7715125865aa05ff88e1c0e9b00ae67a23823
Oteri Prince <oteriprince01@gmail.com>
2024-09-30 13:09:28 +0100

    vite update

--- PinPoint-ui/vite.config.js
+++ PinPoint-ui/vite.config.js
@@ -10,15 +10,16 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          // This example splits React and React DOM into a separate chunk
-          react: ['react', 'react-dom'],
-          // You can add more chunks for other large dependencies
-          vendor: ['axios', 'lodash'], // Example for other dependencies
+          // React-related libraries in one chunk
+          react: ['react', 'react-dom', 'react-router-dom', 'react-icons'],
+          // Axios and Swiper in another chunk
+          utilities: ['axios', 'swiper'],
+          // Chunk for Leaflet-related libraries
+          leaflet: ['react-leaflet'],
+          // Chunk for other smaller or custom libraries
+          custom: ['@custom-react-hooks/use-media-query', 'infinite-react-carousel', 'sass'],
         },
       },
     },
   },
 });
-
-
-
