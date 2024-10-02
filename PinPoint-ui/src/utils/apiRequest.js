d8c0b7e83ed4004554797f024cbc38063f827a33
Oteri Prince <oteriprince01@gmail.com>
2024-10-02 13:28:29 +0100

    update

--- PinPoint-ui/src/utils/apiRequest.js
+++ PinPoint-ui/src/utils/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://pinpoint-api.onrender.com",
+  baseURL: "https://pinpoint-api.onrender.com/api",
   withCredentials: true,
 });
 
